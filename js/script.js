9b9dc85ee1ffdd1b3373b4571f5459f5604eb3f2
Ircham Mudzakir <irchamudzakir@gmail.com>
2025-01-17 00:43:28 +0700

    Up Porto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website Loaded");
+});
+
+    document.querySelector('#Home').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+
+      document.querySelector('#Portfolio').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
